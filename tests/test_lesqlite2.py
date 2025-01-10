18e993af7fea59d3dbc222922cd7d480068e4c03
abelkov <abelkov@aligntech.com>
2025-01-10 18:28:59 +0400

    un commented test params for lesqlite 2 check

--- tests/test_lesqlite2.py
+++ tests/test_lesqlite2.py
@@ -5,18 +5,18 @@ import pytest
 from src.VarInt import LeSQLite2
 
 PARAMS = [
-    # [b'\x00', 0],
-    # [b'\xb1', 177],
-    # [b'\xb2\x00', 178],
-    # [b'\xf1\xff', 16561],
-    # [b'\xf2\x00\x00', 16562],
+    [b'\x00', 0],
+    [b'\xb1', 177],
+    [b'\xb2\x00', 178],
+    [b'\xf1\xff', 16561],
+    [b'\xf2\x00\x00', 16562],
     [b'\xf9\xbfM', 524287],
-    # [b'\xfa\x00\x00\x08', 524288],
-    # [b'\xfb\xff\xff\xff\xff', 4294967295],
-    # [b'\xfc\xff\xff\xff\xff\xff', 1099511627775],
-    # [b'\xfd\xff\xff\xff\xff\xff\xff', 281474976710655],
-    # [b'\xfe\xff\xff\xff\xff\xff\xff\xff', 72057594037927935],
-    # [b'\xff\x00\x00\x00\x00\x00\x00\x00\x01', 72057594037927936]
+    [b'\xfa\x00\x00\x08', 524288],
+    [b'\xfb\xff\xff\xff\xff', 4294967295],
+    [b'\xfc\xff\xff\xff\xff\xff', 1099511627775],
+    [b'\xfd\xff\xff\xff\xff\xff\xff', 281474976710655],
+    [b'\xfe\xff\xff\xff\xff\xff\xff\xff', 72057594037927935],
+    [b'\xff\x00\x00\x00\x00\x00\x00\x00\x01', 72057594037927936]
 ]
 
 
